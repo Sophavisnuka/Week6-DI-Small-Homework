f6ae85837376faff12890b438849398ffff45af1
Visnuka <168633610+Sophavisnuka@users.noreply.github.com>
2026-02-25 14:08:32 +0700

    Implement the main_production

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -3,11 +3,11 @@ import 'package:flutter_all_exercise/data/repositories/songs/song_repository_rem
 import 'package:flutter_all_exercise/main_common.dart';
 import 'package:provider/provider.dart';
 
-List<Provider> get production {
+List<Provider> get providerProduction {
   return [
     Provider<SongRepository>(create: (context) => SongRepositoryRemote())
   ];
 }
 void main() {
-  mainCommon(production);
+  mainCommon(providerProduction);
 }
