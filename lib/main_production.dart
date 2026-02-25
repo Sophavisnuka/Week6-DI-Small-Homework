06aa69cfe1d19d4a3272d5ab566d0cc9ec4c12ee
Visnuka <168633610+Sophavisnuka@users.noreply.github.com>
2026-02-25 14:07:05 +0700

    Initial flutter

---
+++ lib/main_production.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_all_exercise/data/repositories/songs/song_repository.dart';
+import 'package:flutter_all_exercise/data/repositories/songs/song_repository_remote.dart';
+import 'package:flutter_all_exercise/main_common.dart';
+import 'package:provider/provider.dart';
+
+List<Provider> get production {
+  return [
+    Provider<SongRepository>(create: (context) => SongRepositoryRemote())
+  ];
+}
+void main() {
+  mainCommon(production);
+}
